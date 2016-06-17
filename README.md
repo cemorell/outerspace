ad8ad647b53e94be0c1416ab11f4554eda40c907
camille morell <cemorell>
2016-06-17 16:18:30 -0500

    done for now

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # outerspace
+
+This CLI node / API  prompts a date from 1996 to the present to explore that day in space!
+
+run in node nasa.js
--- nasa.js
+++ nasa.js
@@ -1,17 +1,17 @@
 var prompt = require('prompt');
 var open = require('open');
 var request = require('request');
-
+const delay = require('delay');
 
 
 // Say hello
-console.log("Welcome to the Space Jam please pick the YYYY-MM-DD you want to travel to (after 1996)!");
-// prompt user for AP
+console.log("Can you handle the Space Jam? Pick a date (after 1996)");
 prompt.start();
-prompt.get(['setDate'], function (err, result) {
-  console.log('Off to ' + result.setDate + ' away we go!')
-  var nasaThing = new Nasapic(result.setDate);
+prompt.get(['findinfo'], function (err, result) {
+  console.log('Off to ' + result.findinfo + ' away we go!')
+  var nasaThing = new Nasapic(result.findinfo);
 });
+
 
 function Nasapic(picDate){
  this.picDate = picDate;
@@ -23,11 +23,19 @@ Nasapic.prototype.setDate = function(){
  var self = this;
  request('https://api.nasa.gov/planetary/apod?api_key=tK09tzi66i3PQVen42IpjxYqDcPb7eN9biLCuehB&date=' + encodeURIComponent(this.picDate), function (error, response, body) {
    if (!error && response.statusCode == 200) {
-     console.log(typeof body)
      var bodyObject = JSON.parse(body);
-     console.log(typeof bodyObject)
-     var image = bodyObject.url;
-     open(image);
+     console.log(bodyObject.title);
+     console.log(bodyObject.explanation);
+     var image = bodyObject.hdurl;
+      delay(9000)
+  .then(() => {
+      open('https://www.youtube.com/watch?v=J9FImc2LOr8');
+      delay(2300)
+  .then(() => {
+      open(image);
+    });
+  });
+
    };
  });
 
